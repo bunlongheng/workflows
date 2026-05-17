59e771211890e25ea9d8ecb4b84df2de3f1f5154
Bunlong Heng <bheng.code@gmail.com>
2026-05-17 15:34:55 -0400

    chore: migrate next lint -> eslint cli with next 16 plugin
    - Generated eslint.config.mjs via @next/codemod next-lint-to-eslint-cli
    - Imports eslint-config-next/core-web-vitals + /typescript (wraps
      @next/eslint-plugin-next recommended rules)
    - Adds eslint + eslint-config-next devDependencies
    - package.json scripts: lint -> eslint .
    - Honors underscore-prefix convention for intentionally unused vars/args
    - 0 errors, 31 warnings (mostly <img> -> next/image suggestions); lint
      exits 0

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    rules: {
+      // Honor underscore-prefix convention for intentionally unused vars / args.
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
+
+export default eslintConfig;
